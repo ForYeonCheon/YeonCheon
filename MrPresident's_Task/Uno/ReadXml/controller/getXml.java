71902ee87a83257fb344d27da4df95b1a8c88fac
uno-kim <zhfldk014745@naver.com>
2022-04-21 19:53:20 +0900

    :tada: create login method

---
+++ MrPresident's_Task/Uno/ReadXml/controller/getXml.java
@@ -0,0 +1,16 @@
+package Uno.ReadXml.controller;
+
+import java.util.Map;
+
+public class getXml {
+	private Map<String, Map<String, String>> userInfo;
+
+	public Map<String, Map<String, String>> getUserInfo() {
+		return userInfo;
+	}
+
+	public void setUserInfo(Map<String, Map<String, String>> userInfo) {
+		this.userInfo = userInfo;
+	}
+
+}
--- MrPresident's_Task/Uno/ReadXml/starter.java
+++ MrPresident's_Task/Uno/ReadXml/starter.java
@@ -14,6 +14,8 @@ import org.w3c.dom.NodeList;
 public class starter {
 
 	public static void main(String[] args) throws Exception {
+		starter starter = new starter();
+		
 		// TODO Auto-generated method stub
 		try {
 			String directoryName = System.getProperty("user.dir") + "\\MrPresident's_Task\\Uno\\ReadXml\\XmlFiles\\";
@@ -23,26 +25,58 @@ public class starter {
 			Document document = db.parse(file);
 			document.getDocumentElement().normalize();
 			System.out.println("Root Element :" + document.getDocumentElement().getNodeName());
-			NodeList nList = document.getElementsByTagName("employee");
+			NodeList nList = document.getElementsByTagName("userId");
 			System.out.println("----------------------------");
-			for (int temp = 0; temp < nList.getLength(); temp++) {
-				Node nNode = nList.item(temp);
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element eElement = (Element) nNode;
-					if (eElement.getAttribute("id").equals("1001")) {
-						System.out.println("Employee id : " + eElement.getAttribute("id"));
-						System.out.println(
-								"First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
-						System.out.println(
-								"Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
-						System.out.println(
-								"Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
-					}
-				}
+			String id = null;
+			String password = null;
+			if (starter.checkUser(nList, id, password)) {
+				System.out.println("로그인 성공");
+			} else {
+				System.out.println("로그인 실패");
 			}
+//			starter.checkUser(nList, id, password);
+//			for (int temp = 0; temp < nList.getLength(); temp++) {
+//				Node nNode = nList.item(temp);
+//				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+//					Element eElement = (Element) nNode;
+//					if (eElement.getAttribute("id").equals("koria189")) {
+//						System.out.println("Employee id : " + eElement.getAttribute("id"));
+//						System.out.println(
+//								"First Name : " + eElement.getElementsByTagName("password").item(0).getTextContent());
+//						System.out.println(
+//								"Last Name : " + eElement.getElementsByTagName("name").item(0).getTextContent());
+//						System.out.println(
+//								"Salary : " + eElement.getElementsByTagName("server").item(0).getTextContent());
+//					}
+//				}
+//			}
 		} catch (IOException e) {
 			System.out.println(e);
 		}
+	}
+
+	boolean checkUser(final NodeList nList, String id, String passWord) {
+		id = "koria189";
+		passWord = "test1234";
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+			Node nNode = nList.item(temp);
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+				if (eElement.getAttribute("id").equals(id)) {
+					System.out.println("user id : " + eElement.getAttribute("id"));
+					checkPassword(eElement, passWord);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	boolean checkPassword(Element eElement, String passWord) {
+		if (eElement.getElementsByTagName("password").item(0).getTextContent().equals(passWord)) {
+			return true;
+		}
+		return false;
 	}
 
 }
