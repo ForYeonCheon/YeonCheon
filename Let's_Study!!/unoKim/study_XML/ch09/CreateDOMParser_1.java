fa50c768b393da607870d08b81d08df18a57033f
uno-kim <zhfldk014745@naver.com>
2022-05-06 20:40:36 +0900

    :sparkles: success craeetedom parser

--- Let's_Study!!/unoKim/study_XML/ch09/CreateDOMParser_1.java
+++ Let's_Study!!/unoKim/study_XML/ch09/CreateDOMParser_1.java
@@ -1,6 +1,23 @@
 package unoKim.study_XML.ch09;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public class CreateDOMParser_1
 {
+	public static void main(String[] args)
+	{
+		// DOM 파서 공장 생성
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try
+		{
+			DocumentBuilder parser = factory.newDocumentBuilder();
+		} catch (Exception e)
+		{
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		System.out.println("Dom 파서 객체 생성 성공");
+	}
 
 }
